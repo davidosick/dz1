31e5b1899a930c3e2b77989c6284414c983a0726
redfortf <150962718+redfortf@users.noreply.github.com>
2024-04-27 00:12:10 +0300

    Optimise and prettify code a bit

--- temp.py
+++ temp.py
@@ -1,37 +1,30 @@
 import tkinter as tk
 from tkinter import filedialog
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.widgets import Slider
 
-def load_file():
-    global numbers
+def load_file() -> None:
     filepath = filedialog.askopenfilename(filetypes=[("TXT файлы", "*.txt")])
     if filepath:
         with open(filepath, "r") as file:
-            text = file.read()
+            plot_data([
+                float(num.replace(",", ".")) 
+                for line in file.readlines() if line.strip() 
+                for num in line.split("\t")
+            ])
 
-        lines = text.split('\n')
-
-        numbers = []
-
-        for line in lines:
-            if not line.strip():
-                continue
-            numbers.extend([float(num.replace(',', '.')) for num in line.split('\t')])
-
-        plot_data(numbers)
-
-def plot_data(data):
+def plot_data(data: list[float]) -> None:
     plt.clf()
     plt.plot(data)
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.xlabel("X")
+    plt.ylabel("Y")
     plt.legend()
     canvas.draw()
 
-def update(val):
-    ax.set_xlim(slider.val, slider.val + 10)  
+def slider_on_change_handler(val: float) -> None:
+    ax.set_xlim(val, val + 10)  
     canvas.draw_idle()
 
 root = tk.Tk()
@@ -44,8 +37,8 @@ fig, ax = plt.subplots()
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-slider_ax = plt.axes([0.1, 0.02, 0.8, 0.03])  
-slider = Slider(slider_ax, 'X', 0, 130000, valinit=0)  
-slider.on_changed(update)  
+slider_ax = plt.axes((0.1, 0.02, 0.8, 0.03))  
+slider = Slider(slider_ax, "X", 0, 130000, valinit=0)  
+slider.on_changed(slider_on_change_handler)
 
-tk.mainloop()
+root.mainloop()
