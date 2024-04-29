2e750fbd6fad48e5548619f8ec17661f47b142f9
davidosick <48261644+davidosick@users.noreply.github.com>
2024-04-29 21:54:44 +0300

    раздельная подгрузка

--- temp.py
+++ temp.py
@@ -3,44 +3,66 @@ from tkinter import filedialog
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.widgets import Slider
+#from matplotlib.widgets import Slider
 
-def load_file() -> None:
+global voltage_numbers, ampere_numbers
+
+def load_file_voltage() -> None:
+    data = load_file()
+
+    voltage_numbers = [[float(num.replace(',', '.')) 
+                        for num in line.split('\t')] 
+                        for line in data]
+
+    plot_data(voltage_numbers)
+
+def load_file_ampere() -> None:
+    data = load_file()
+
+    ampere_numbers = [[float(num.replace(',', '.')) 
+                        for num in line.split('\t')] 
+                        for line in data]
+
+    plot_data(ampere_numbers)
+
+def load_file():
     filepath = filedialog.askopenfilename(filetypes=[("TXT файлы", "*.txt")])
     if filepath:
         with open(filepath, "r") as file:
             text = file.read()
 
         data = text.strip().split('\n')
-
-        plot_data([[float(num.replace(',', '.')) 
-                    for num in line.split('\t')] 
-                    for line in data])
+        return data
 
 def plot_data(data: list[float]) -> None:
-    plt.clf()
     plt.plot(data)
-    plt.xlabel("X")
-    plt.ylabel("Y")
     plt.legend()
     canvas.draw()
 
-def slider_on_change_handler(val: float) -> None:
+'''def slider_on_change_handler(val: float) -> None:
     ax.set_xlim(val, val + 10)  
-    canvas.draw_idle()
+    canvas.draw_idle()'''
 
 root = tk.Tk()
 root.title("ДЗ1 by ТыШаКаТя")
 
-load_button = tk.Button(root, text="Выбрать файл", command=load_file)
-load_button.pack()
+plt.clf()
+
+load_button_voltage = tk.Button(root, text="Напряжение (U)", command=load_file_voltage)
+load_button_voltage.pack()
+
+load_button_voltage = tk.Button(root, text="Сила тока (I)", command=load_file_ampere)
+load_button_voltage.pack()
 
 fig, ax = plt.subplots()
+ax.grid(True)
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+plt.xlabel("X")
+plt.ylabel("Y")
 
-slider_ax = plt.axes((0.1, 0.02, 0.8, 0.03))  
+'''slider_ax = plt.axes((0.1, 0.02, 0.8, 0.03))  
 slider = Slider(slider_ax, "X", 0, 130000, valinit=0)  
-slider.on_changed(slider_on_change_handler)
+slider.on_changed(slider_on_change_handler)'''
 
 root.mainloop()
