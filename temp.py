aae2f85745a97c02b4448229b2b347fc03005790
redfortf <150962718+redfortf@users.noreply.github.com>
2024-04-30 19:27:34 +0300

    Добавил вывод времени эксперимента

--- temp.py
+++ temp.py
@@ -1,57 +1,71 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk 
+from tkinter import Event, filedialog
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tkinter import ttk 
 #from matplotlib.widgets import Slider
 
 global voltage_numbers, ampere_numbers
+global voltage_numbers_time, ampere_numbers_time
+
+def timeformat(full_seconds: float) -> str:
+    full_minutes = int(full_seconds // 60)
+    full_hours   = int(full_minutes // 60)
+    
+    seconds = full_seconds % 60
+    minutes = full_minutes % 60
+    return f"{full_hours:02}:{minutes:02}:{seconds:.2f}"
+
+def parse_file_lines(lines: list[str]) -> list[list[float]]:
+    return [[float(num.replace(',', '.'))
+             for num in line.split('\t')] 
+             for i, line in enumerate(lines) if i % 2 == 0]
 
 def load_file_voltage() -> None:
-    global voltage_numbers
-    data = load_file()
-
-    voltage_numbers = [[float(num.replace(',', '.')) 
-                        for num in line.split('\t')] 
-                        for line in data]
-
-    plot_data(voltage_numbers)
-    combobox.current(0)
+    global voltage_numbers, voltage_numbers_time
+    if data := load_file():
+        voltage_numbers = parse_file_lines(data)
+        voltage_numbers_time = timeformat(len(data) / 10)
+        plot_data(voltage_numbers)
+        label.configure(text=str(voltage_numbers_time))
+        combobox.current(0)
 
 def load_file_ampere() -> None:
-    global ampere_numbers
-    data = load_file()
+    global ampere_numbers, ampere_numbers_time
+    if data := load_file():
+        ampere_numbers = parse_file_lines(data)
+        ampere_numbers_time = timeformat(len(data) / 10)
+        plot_data(ampere_numbers)
+        label.configure(text=str(ampere_numbers_time))
+        combobox.current(1)
 
-    ampere_numbers = [[float(num.replace(',', '.')) 
-                        for num in line.split('\t')] 
-                        for line in data]
-
-    plot_data(ampere_numbers)
-    combobox.current(1)
-
-def load_file():
+def load_file() -> list[str] | None:
     filepath = filedialog.askopenfilename(filetypes=[("TXT файлы", "*.txt")])
     if filepath:
         with open(filepath, "r") as file:
             text = file.read()
 
-        data = text.strip().split('\n')
-        return data
+        return text.strip().split('\n')
 
-def plot_data(data: list[float]) -> None:
+def plot_data(data: list[list[float]]) -> None:
     plt.clf()
     plt.plot(data)
-    plt.legend()
+    ax.grid(True)
+    plt.xlabel("X")
+    plt.ylabel("Y")
     canvas.draw()
 
 def select_graph(event):
     global voltage_numbers, ampere_numbers
-    selection = combobox.current() 
-    if selection == 0:
-        plot_data(voltage_numbers)
-    elif selection == 1:
-        plot_data(ampere_numbers)
+    global voltage_numbers_time, ampere_numbers_time
+    match combobox.current():
+        case 0 if voltage_numbers:
+            plot_data(voltage_numbers)
+            label.configure(text=f"{label_text} {voltage_numbers_time}")
+        case 1 if ampere_numbers:
+            plot_data(ampere_numbers)
+            label.configure(text=f"{label_text} {ampere_numbers_time}")
 
 '''def slider_on_change_handler(val: float) -> None:
     ax.set_xlim(val, val + 10)  
@@ -77,10 +91,14 @@ combobox.bind("<<ComboboxSelected>>", select_graph)
 
 fig, ax = plt.subplots()
 ax.grid(True)
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 plt.xlabel("X")
 plt.ylabel("Y")
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+
+label_text = "Время: "
+label = ttk.Label(root, text=f"{label_text}")
+label.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.TRUE)
 
 '''slider_ax = plt.axes((0.1, 0.02, 0.8, 0.03))  
 slider = Slider(slider_ax, "X", 0, 130000, valinit=0)  
