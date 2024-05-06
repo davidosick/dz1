44f76e3317fc368b1f40f245a057e7bbf154e9e1
Tyagelsky Artyom <klassnyjartem8@gmail.com>
2024-05-06 20:22:48 +0600

    Code comments are added

--- temp.py
+++ temp.py
@@ -15,7 +15,6 @@ class ComboboxSelection:
     REACTIVE_POWER = 4
     FULL_POWER = 5
 
-
     all = [0, 1, 2, 3, 4, 5]
 
 
@@ -54,6 +53,8 @@ global selected_items
 selected_items: list[int]
 
 
+# consume time in seconds and represents it in string
+# that consists of hours, minutes and seconds.
 def time_format(full_seconds: float) -> str:
     if full_seconds == 0:
         return "нет"
@@ -66,23 +67,31 @@ def time_format(full_seconds: float) -> str:
     return f"{full_hours:02}:{minutes:02}:{seconds:.2f}"
 
 
+# reads data from file, that we may peak in filedialog menu
+# returns list of strings (or null)
 def load_file() -> list[str] | None:
     if filepath := filedialog.askopenfilename(filetypes=[("TXT файлы", "*.txt")]):
         with open(filepath, "r") as file:
             return file.readlines()
 
 
+# this method parses given list and fetches elements in
+# even lines (that are not duplicates)
+
+# returns 2-dimensional array of float numbers
 def parse_file_lines(lines: list[str]) -> list[list[float]]:
-    return [[float(num.replace(',', '.'))
+    return [[float(num.replace(',', '.')) # substitutes commas by dots
              for num in line.split('\t')]
             for i, line in enumerate(lines) if i % 2 == 0]
+
 
 
 def load_file_voltage() -> None:
     global NUMBERS
     if data := load_file():
         NUMBERS[ComboboxSelection.VOLTAGE] = parse_file_lines(data)
-
+        # combobox.current(int newindex) sets the combobox
+        # value to the element at position newindex in the list of values
         combobox.current(ComboboxSelection.VOLTAGE)
         clear_scene()
 
@@ -109,11 +118,14 @@ def load_file_instant_power() -> None:
         clear_scene()
 
 
+# calculates instant power elements by multiplication of voltage and current
+# getting 2 lists -> one list of multiplication numbers
 def calculate_instant_power(voltage_numbers: list[list[float]], 
                             current_numbers: list[list[float]]) -> list[list[float]]:
     return [[voltage * current
              for voltage, current in zip(voltage_data, current_data)]
             for voltage_data, current_data in zip(voltage_numbers, current_numbers)]
+
 
 def load_file_active_power() -> None:
     global NUMBERS
@@ -122,6 +134,10 @@ def load_file_active_power() -> None:
     if len(current_numbers) <= 0 or len(voltage_numbers) <= 0:
         messagebox.showerror("Ошибка", "Вы не загрузили значения напряжения или силы тока")
     else:
+        #if len(NUMBERS[ComboboxSelection.INSTANT_POWER]) <= 0
+        # NUMBERS[ComboboxSelection.INSTANT_POWER] = calculate_instant_power(voltage_numbers, current_numbers)
+        #else
+        # @delete underlying line@#
         NUMBERS[ComboboxSelection.INSTANT_POWER] = calculate_instant_power(voltage_numbers, current_numbers)
         NUMBERS[ComboboxSelection.ACTIVE_POWER] = calculate_active_power(NUMBERS[ComboboxSelection.INSTANT_POWER])
 
@@ -180,15 +196,21 @@ def calculate_full_power(voltage_numbers: list[list[float]],
             for voltage_data, current_data in zip(voltage_numbers, current_numbers)]
 
 
+# draw new thing on canvas, consists of 2 steps
+# 1. Clear the canvas and create description for axis, updates time, etc.
+# 2. Draw new chart
 def clear_scene() -> None:
     update_scene()
-    plot_data(selected_items)  # просто чистим холст
+    plot_data(selected_items)
 
 
+# prepares the diagram description and updates time
 def update_scene() -> None:
     update_graph_list()
     update_time()
 
+# clears selected items for drawing,
+                        #  clears the scene and puts new X-axis description
 def update_graph_list() -> None:
     global selected_items, experiment_time
     selected_items = []
@@ -199,18 +221,23 @@ def update_graph_list() -> None:
         tree.insert("", tk.END, text=f"График {LABELS[combobox.current()]}({i + 1})")
 
 
+# frequency = 800 measures/second, 80 measures in every line
+# -> 10 lines = 1 second -> experiment time = number of lines / 10.
+# + data formatting
 def update_time() -> None:
     experiment_time = len(NUMBERS[combobox.current()]) / 10
     time_label.configure(text=f"Время: {time_format(experiment_time)}")
 
 
+# clear the canvas and draw new element(s)
 def plot_data(data: list[int]) -> list[Line2D]:
+    # removes the figure
     plt.clf()
 
     if len(data) <= 0:
         line_objects = []
     else:
-        time_seconds = [i / 10 / 80 for i in range(80)]  # из задания длина всегда 80
+        time_seconds = [i / 10 / 80 for i in range(80)]  # length of the row is always 80
         line_objects = [plt.plot(time_seconds, NUMBERS[combobox.current()][i],
                                  label=f"{LABELS[combobox.current()]}({i + 1})")[0]
                         for i in data]
@@ -263,12 +290,16 @@ def set_plot():
     plt.ylabel(LABELS[combobox.current()])
 
 
+#crating main window
+#root is a reference to manipulate main frame
 root = tk.Tk()
 root.title("ДЗ1 by ТыШаКаТя")
 
+#put the frame for buttons inside parent's top-level widget
 button_frame = tk.Frame(root)
 button_frame.pack(side=tk.TOP)
 
+#creating buttons for each operation
 for i, command in {
     ComboboxSelection.VOLTAGE: load_file_voltage,
     ComboboxSelection.CURRENT: load_file_current,
@@ -280,33 +311,50 @@ for i, command in {
     load_button = tk.Button(button_frame, command=command, text=f"+ {TITLES[i]} ({LABELS[i]})")
     load_button.pack(side=tk.LEFT, padx=5)
 
+#Combobox is a widget that combines a text field
+# with a pop-down list of values
 combobox = ttk.Combobox(root, state="readonly", values=[f"{TITLES[i]} ({LABELS[i]})" for i in ComboboxSelection.all])
+
+# by default the combobox is specified by voltage int representation
 combobox.current(ComboboxSelection.VOLTAGE)
 combobox.pack()
 combobox.bind("<<ComboboxSelected>>", combobox_on_select)
 
+#
 tree_frame = tk.Frame(root)
 tree_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.TRUE)
 
+#creation of button for clearing the scene and selected chart lines to display
 clear_button = tk.Button(tree_frame, text="Очистить выбор", command=clear_on_select)
 clear_button.pack(side=tk.TOP)
 
+#creation of list of views for every line that chart can be displayed
 tree = ttk.Treeview(tree_frame, selectmode=tk.EXTENDED)
 tree.heading("#0", text="Выбор графиков:")
 tree.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.TRUE)
 
+#creation of scrollbar
 scrollbar = ttk.Scrollbar(tree_frame, orient=tk.VERTICAL, command=tree.yview)
 scrollbar.pack(side=tk.LEFT, fill=tk.Y, anchor=tk.W)
 
 tree.configure(yscrollcommand=scrollbar.set)
 tree.bind("<<TreeviewSelect>>", tree_on_select)
 
+###paint bars using matplotlib dependency
+
+#create a figure
 fig = plt.figure()
+
+#clear the plot and set appropriate labels to axes, etc
 set_plot()
+
+#canvas that is used to be painted on
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.TRUE)
 
+#time label
 time_label = ttk.Label(root, text=f"Время: {time_format(0)}")
 time_label.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.TRUE)
 
+# starting our frame application
 root.mainloop()
